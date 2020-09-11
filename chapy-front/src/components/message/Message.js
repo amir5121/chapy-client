ab48836842fd10c319342dfa85e5cbba13824e54
amirhosein <amirhoseinheshmati@gmail.com>
2020-09-11 11:22:12 +0430

    cost

--- chapy-front/src/components/message/Message.js
+++ chapy-front/src/components/message/Message.js
@@ -2,16 +2,42 @@ import React from "react";
 import "./Message.less";
 import { Row } from "antd";
 import { toPersian } from "../../utils/DateConvertor";
+import { CheckCircleFilled } from "@ant-design/icons";
 
 const Message = (props) => {
-  const { text, created, is_mine, cost } = props;
+  const {
+    text,
+    created,
+    is_mine,
+    cost,
+    need_payment,
+    acceptCharge,
+    identifier,
+  } = props;
   return (
     <Row className="chat-row" justify={is_mine ? "start" : "end"}>
       <div className="speech-bubble">
-        <p className="chat-content">{text}</p>
+        {need_payment ? (
+          <div>
+            <p>{`accept the ${cost} charge?`}</p>
+            <CheckCircleFilled
+              style={{
+                fontSize: "2em",
+                color: "limegreen",
+                width: "100%",
+                marginBottom: "0.5em",
+              }}
+              twoToneColor="#52c41a"
+              onClick={() => acceptCharge(identifier)}
+            />
+          </div>
+        ) : (
+          <p className="chat-content">{text}</p>
+        )}
+
         <p className="chat-date">
           <span>{toPersian(created)}</span>
-          {cost && <span>&nbsp;${cost}</span>}
+          {!need_payment && cost && <span>&nbsp;${cost}</span>}
         </p>
       </div>
     </Row>
